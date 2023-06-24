ea83e87803d179fb09303584c4a6a13cc1e85a0c
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-24 21:45:28 +0530

    Add Lec_3_Q_12

---
+++ GUI/Lec_3_Q_12/Lec_3_Q_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_3_Q_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
