9961ed9aaea3a506f6769236c83a76f144e1414a
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-25 00:00:18 +0530

    Add Lec_3_Q_14, Class created in Traint, Created 3 methods,           input();           purchaseTicketst();           findFinalResult();           incomes(); use parameterize constructor, method to purchaseTickets by accept marks as paramenters and call all the methods in the main method.

---
+++ GUI/Lec_3_Q_14/Lec_3_Q_14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_3_Q_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Train Name or Number: ");
+            string n=Console.ReadLine();
+
+            Console.Write("Enter the Destination: ");
+            string d=Console.ReadLine();
+
+            Console.Write("Enter the Driver Name: ");
+            string dn=Console.ReadLine();
+
+            Train t = new Train(n,d,dn);
+            t.inputs();
+
+            Console.Write("Enter the Resever Tickets: ");
+            int rt=Convert.ToInt32(Console.ReadLine());
+
+            t.purchaseTickets(rt);
+            t.incomes();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GUI/Lec_3_Q_14/Lec_3_Q_14/Train.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_3_Q_14
+{
+    internal class Train
+    {
+        private string name;
+        private string destination;
+        private string driverName;
+        private int[] apartment = new int[10];
+        private int totSeats;
+        private int reserveTickets;
+
+
+        public Train(string n, string des, string dn)
+        {
+            name = n;
+            destination = des;
+            driverName = dn;
+        }
+
+        public void inputs()
+        {
+            for(int c=0;c<apartment.Length;c++)
+            {
+                Console.Write("Enter the Capasity of Apartment: ");
+                apartment[c]=Convert.ToInt32(Console.ReadLine());
+            }
+            for (int c = 0; c < apartment.Length; c++)
+            {
+                totSeats += apartment[c];
+            }
+        }
+
+        public void purchaseTickets(int rt)
+        {
+            reserveTickets = rt;
+            int available = totSeats - reserveTickets;
+            Console.WriteLine("Available Seats: " + available);
+        }
+
+        public void incomes()
+        {
+            int income = reserveTickets * 400;
+            Console.WriteLine("Expected Income: " + income);
+        }
+    }
+}
