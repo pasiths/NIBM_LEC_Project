e1d903280dbd759e8aeeb315d572b37a8da51214
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-24 23:17:27 +0530

    Add Lec_3_Q_13, Class created in Student, Created 4 methods,           input();           calAssigment();           findFinalResult();           display(); method to calAverage by accept marks as paramenters and call all the methods in the main method

---
+++ GUI/Lec_3_Q_13/Lec_3_Q_13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_3_Q_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            st.input();
+
+            Console.Write("Enter the Marks of Exam: ");
+            int exam = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Marks of Assigment: ");
+            int assigmnet= Convert.ToInt32(Console.ReadLine());
+
+            st.calAverage(exam, assigmnet);
+            st.findFinalresult();
+            st.display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GUI/Lec_3_Q_13/Lec_3_Q_13/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_3_Q_13
+{
+    internal class Student
+    {
+        public string name;
+        public double average;
+        public string finalResult;
+
+        public void input()
+        {
+            Console.Write("Enter the name: ");
+            name= Console.ReadLine();
+        }
+
+        public void calAverage(int exam,int assigment)
+        {
+            average = (exam * assigment);
+        }
+
+        public void findFinalresult()
+        {
+            if (average >= 60)
+            {
+                finalResult = "Pass";
+            }
+            else
+            {
+                finalResult = "Fail";
+            }
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Final Result: " + finalResult);
+        }
+    }
+}
