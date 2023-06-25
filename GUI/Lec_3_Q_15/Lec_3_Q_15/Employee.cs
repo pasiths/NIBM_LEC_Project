76f5255b5a1408d71154f52e027c4eb79651d322
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-25 10:03:55 +0530

    Add Lec_3_Q_15, Class created in Employee, Created 3 methods,           OTH(int o);           calFinalSalary();           returnFinalSalary()); use parameterize constructor, method to OTH by accept marks as paramenters, method to return final Salary and call all the methods in the main method.

---
+++ GUI/Lec_3_Q_15/Lec_3_Q_15/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_3_Q_15
+{
+    internal class Employee
+    {
+        private string name;
+        private int basicSalary;
+        private int oth;
+        private int finalSalary;
+
+        public Employee(string n, int bs)
+        {
+            name = n;
+            basicSalary = bs;
+            oth = 0;
+            finalSalary = 0;
+        }
+
+        public void OTH(int o)
+        {
+            oth = o;
+        }
+
+        public void calFinalSalary()
+        {
+
+            finalSalary = basicSalary + (oth * 2000);
+        }
+
+        public int returnFinalSalary()
+        {
+            return finalSalary;
+        }
+    }
+}
---
+++ GUI/Lec_3_Q_15/Lec_3_Q_15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_3_Q_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Name: ");
+            string n = Console.ReadLine();
+
+            Console.Write("Enter the Basic Salary: ");
+            int bs=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the OT Hours: ");
+            int o = Convert.ToInt32(Console.ReadLine());
+
+            Employee emp = new Employee(n, bs);
+            emp.OTH(o);
+            emp.calFinalSalary();
+
+            Console.WriteLine("Final Salary: "+emp.returnFinalSalary());
+
+            Console.ReadKey();
+        }
+    }
+}
