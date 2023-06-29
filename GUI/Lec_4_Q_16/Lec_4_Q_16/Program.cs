f9a906873b9e17a64496db461ecb82de09ba51dc
Pasith <pasiths@users.noreply.github.com>
2023-06-29 23:09:37 +0530

    .

---
+++ GUI/Lec_4_Q_16/Lec_4_Q_16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_4_Q_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ GUI/Lec_4_Q_16/Lec_4_Q_16/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_4_Q_16
+{
+    internal class Student
+    {
+        private string name;
+        private double avg;
+        private string fr;
+
+        public string name
+        {
+            get { return name; }
+        }
+    }
+}
