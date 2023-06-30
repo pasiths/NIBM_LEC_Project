89c10fc54cd72be127ac8fae560b26ba9f6540b4
Pasith <pasiths@users.noreply.github.com>
2023-06-30 12:16:26 +0530

    06/30 02

---
+++ GUI/justNow630_/justNow630_/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace justNow630_
+{
+    internal class Class1
+    {
+        private string name;
+        private string fr;
+        public string studentName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string finalResult
+        {
+            get { return fr; }
+            set { fr = value; }
+        }
+
+        public Class1()
+        {
+            name = "";
+            finalResult = "Fail";
+        }
+
+
+        public void findResult(int exam,int cw1,int cw2)
+        {
+            double avg=(exam+cw1+cw2)/3;
+            if (avg >= 60)
+            {
+                finalResult = "D";
+            }
+            else if(avg >= 40)
+            {
+                finalResult = "G P";
+            }
+            else
+            {
+                finalResult = "F";
+            }
+        }
+
+        public void findResult(int exam)
+        {
+            if(exam >= 50)
+            {
+                finalResult = "Pass";
+            }
+            else
+            {
+                finalResult = "Fail";
+            }
+        }
+    }
+}
---
+++ GUI/justNow630_/justNow630_/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace justNow630_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 st = new Class1();
+            Console.WriteLine("Enter Stidnt name: ");
+            st.studentName=Console.ReadLine();
+
+            Console.WriteLine("Enter Student type: ");
+            string type = Console.ReadLine();
+
+            if (type=="under gradute")
+            {
+                Console.WriteLine("Enter CW1 Marks: ");
+                int cw1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter CW2 Marks: ");
+                int cw2=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Exam Marks: ");
+                int exam = Convert.ToInt32(Console.ReadLine());
+
+                st.findResult(exam, cw1, cw2);
+            }
+            else
+            {
+                Console.WriteLine("Enter Exam Marks: ");
+                int exam=Convert.ToInt32(Console.ReadLine());
+                st.findResult(exam);
+            }
+            Console.WriteLine("Final Result: " + st.studentName);
+
+            Console.ReadKey();
+        }
+    }
+}
