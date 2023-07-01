c4340664e289cb9bb9cd00d035044818e4113ea0
Pasith-Home <pasiya20024@gmail.com>
2023-07-01 17:30:23 +0530

    Lec_4_Q_17

---
+++ GUI/Lec_4_Q_17/Lec_4_Q_17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_4_Q_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Vehicle();
+
+            Console.Write("Enter the Vehicle Number: ");
+            v.vehicleNumber = Console.ReadLine();
+
+            Console.Write("Enter the Parking Slot: ");
+            v.parkingSlot= Console.ReadLine();
+
+            Console.Write("Enter the Paking time: ");
+            v.numberOfMinites = Convert.ToInt32(Console.ReadLine());
+
+            v.cal();
+
+            Console.WriteLine("Fee: " + v.finalFee);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GUI/Lec_4_Q_17/Lec_4_Q_17/Vehicle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_4_Q_17
+{
+    internal class Vehicle
+    {
+        private string number;
+        private string slot;
+        private int nom;
+        private int fee;
+
+        public string vehicleNumber
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public string parkingSlot
+        {
+            get { return slot; }
+            set { slot = value; }
+        }
+
+        public int numberOfMinites
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public int finalFee
+        {
+            get { return fee; }
+        }
+
+        public Vehicle()
+        {
+            number = "N/A";
+            slot = "N/A";
+            nom = 0;
+            fee = 0;
+        }
+
+        public void cal()
+        {
+            int rpm;
+            if (nom > 180)
+            {
+                rpm = 3;
+            }
+            else if(nom>90)
+            {
+                rpm = 2;
+            }
+            else
+            {
+                rpm = 1;
+            }
+
+            fee = nom * rpm;
+        }
+    }
+}
