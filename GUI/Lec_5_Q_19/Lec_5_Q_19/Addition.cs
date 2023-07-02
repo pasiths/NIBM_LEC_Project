eec62052627e4070bc1a32d6f7918aa27d69c96a
Pasith-Home <pasiya20024@gmail.com>
2023-07-02 14:31:17 +0530

    Lec_5_Q_19

---
+++ GUI/Lec_5_Q_19/Lec_5_Q_19/Addition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_5_Q_19
+{
+    internal class Addition
+    {
+        public void Add()
+        {
+            Console.Write("Enter the number one: ");
+            int num1=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the number two: ");
+            int num2=Convert.ToInt32(Console.ReadLine());
+
+            int add = num1 + num2;
+        }
+
+        public void Add(int n1,int n2)
+        {
+            int add=n1+n2;
+        }
+
+        public void Add(string s1,string s2)
+        {
+            string add=s1+s2;
+        }
+
+        public void Add(double n1,double n2)
+        {
+            double add=n1+n2;
+        }
+
+        public void Add(string s1,double n1)
+        {
+            string add = s1 + n1;
+        }
+    }
+}
---
+++ GUI/Lec_5_Q_19/Lec_5_Q_19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_5_Q_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition addition = new Addition();
+            addition.Add();
+            addition.Add(1, 2);
+            addition.Add("NI", "BM");
+            addition.Add(1.00, 2.00);
+            addition.Add("NI", 9.00);
+
+            Console.ReadKey();
+        }
+    }
+}
