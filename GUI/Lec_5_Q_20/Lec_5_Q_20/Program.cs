6556f7bb513b807944c84b70b110d15c67e950fd
Pasith-Home <pasiya20024@gmail.com>
2023-07-05 23:02:54 +0530

    Lec_5_Q_20

---
+++ GUI/Lec_5_Q_20/Lec_5_Q_20/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_5_Q_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+
+            Console.Write("Enter the student name: ");
+            st.Name = Console.ReadLine();
+
+            Console.Write("Enter the Student Type: ");
+            string stype= Console.ReadLine();
+
+            if (stype == "Undergraduate")
+            {
+                Console.Write("Enter the exam marks: ");
+                int exam=Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the Marks of Course Work_1: ");
+                int cw1=Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the Marks of Course work_2: ");
+                int cw2=Convert.ToInt32(Console.ReadLine());
+
+                st.finalResult(exam,cw1,cw2);
+            }
+            else if(stype =="Certificate")
+            {
+                Console.Write("Enter the marks of exam: ");
+                int exam= Convert.ToInt32(Console.ReadLine());
+
+                st.finalResult(exam);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Student Type");
+            }
+
+            Console.WriteLine("Final Result: " + st.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GUI/Lec_5_Q_20/Lec_5_Q_20/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec_5_Q_20
+{
+    internal class Student
+    {
+        private string name;
+        private string result;
+
+        public string Name { set { name = value; }get { return name; } }
+        public string Result { get { return result; } }
+
+        public Student()
+        {
+            name = "N/A";
+            result = "Fail";
+        }
+
+        public void finalResult(int e,int cw1,int cw2)
+        {
+            double avg=(e+cw1+cw2)/3;
+
+            if (avg >= 60)
+            {
+                result = "Distination";
+            }else if(avg >= 40)
+            {
+                result = "Genaral Pass";
+            }
+            else
+            {
+                result = "Fail";
+            }
+        }
+
+        public void finalResult(int e)
+        {
+            if (e >= 50)
+            {
+                result = "Pass";
+            }
+            else
+            {
+                result = "Fail";
+            }
+        }
+    }
+}
