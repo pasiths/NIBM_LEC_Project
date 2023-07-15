efbbbec6ad515673946139a599dab6822fc6fa4e
Pasith-Home <pasiya20024@gmail.com>
2023-07-15 16:40:55 +0530

    Lec_6_Q_25

---
+++ GUI/Lec_6_Q_25/Lec_6_Q_25/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Lec_6_Q_25
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNUm2 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnPer = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnPer);
+            this.panel1.Controls.Add(this.btnMul);
+            this.panel1.Controls.Add(this.btnDiv);
+            this.panel1.Controls.Add(this.btnSub);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txtNum1);
+            this.panel1.Controls.Add(this.txtNum2);
+            this.panel1.Controls.Add(this.lblNUm2);
+            this.panel1.Controls.Add(this.lblNum1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(384, 230);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(30, 39);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(119, 29);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "Number 1";
+            // 
+            // lblNUm2
+            // 
+            this.lblNUm2.AutoSize = true;
+            this.lblNUm2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNUm2.Location = new System.Drawing.Point(30, 81);
+            this.lblNUm2.Name = "lblNUm2";
+            this.lblNUm2.Size = new System.Drawing.Size(119, 29);
+            this.lblNUm2.TabIndex = 1;
+            this.lblNUm2.Text = "Number 2";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(187, 78);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(179, 34);
+            this.txtNum2.TabIndex = 2;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(187, 36);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(179, 34);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(22, 145);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(56, 56);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSub.Location = new System.Drawing.Point(93, 145);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(56, 56);
+            this.btnSub.TabIndex = 5;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMul.Location = new System.Drawing.Point(310, 145);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(56, 56);
+            this.btnMul.TabIndex = 7;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.Location = new System.Drawing.Point(236, 145);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(56, 56);
+            this.btnDiv.TabIndex = 6;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnPer
+            // 
+            this.btnPer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPer.Location = new System.Drawing.Point(165, 145);
+            this.btnPer.Name = "btnPer";
+            this.btnPer.Size = new System.Drawing.Size(56, 56);
+            this.btnPer.TabIndex = 8;
+            this.btnPer.Text = "%";
+            this.btnPer.UseVisualStyleBackColor = true;
+            this.btnPer.Click += new System.EventHandler(this.btnPer_Click);
+            // 
+            // frmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 230);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmCalculator";
+            this.Text = "Calculator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPer;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblNUm2;
+        private System.Windows.Forms.Label lblNum1;
+    }
+}
+
---
+++ GUI/Lec_6_Q_25/Lec_6_Q_25/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lec_6_Q_25
+{
+    public partial class frmCalculator : Form
+    {
+        public frmCalculator()
+        {
+            InitializeComponent();
+        }
+        private double num1;
+        private double num2;
+        private double ans = 0;
+        private void cal(string opcode)
+        {
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+
+            if (opcode == "+")
+            {
+                ans=num1+num2;
+            }
+            else if (opcode == "-")
+            {
+                ans=num1-num2;
+            }
+            else if (opcode == "*")
+            {
+                ans=num1*num2;
+            }
+            else if(opcode == "/")
+            {
+                ans = num1 / num2;
+            }
+            else if (opcode == "%")
+            {
+                ans = num1 % num2;
+            }
+
+            MessageBox.Show("Answer: " + ans);
+        }
+
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            cal("+");
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            cal("-");
+        }
+
+        private void btnPer_Click(object sender, EventArgs e)
+        {
+            cal("%");
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            cal("/");
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            cal("*");
+        }
+    }
+}
