80ec865b810e93a3ddb473c838ecc4bb662049fb
Pasith-Home <pasiya20024@gmail.com>
2023-07-16 03:18:25 +0530

    Lec_6_Q_27

---
+++ GUI/Lec_6_Q_27/Lec_6_Q_27/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Lec_6_Q_27
+{
+    partial class frmBill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.txtUC = new System.Windows.Forms.TextBox();
+            this.lblUC = new System.Windows.Forms.Label();
+            this.txtQ = new System.Windows.Forms.TextBox();
+            this.lblQ = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtAmount);
+            this.panel1.Controls.Add(this.lblAmount);
+            this.panel1.Controls.Add(this.btnClick);
+            this.panel1.Controls.Add(this.txtUC);
+            this.panel1.Controls.Add(this.lblUC);
+            this.panel1.Controls.Add(this.txtQ);
+            this.panel1.Controls.Add(this.lblQ);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(438, 290);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(138, 237);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(271, 34);
+            this.txtAmount.TabIndex = 8;
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.Location = new System.Drawing.Point(29, 240);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(94, 29);
+            this.lblAmount.TabIndex = 7;
+            this.lblAmount.Text = "Amount";
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(89, 179);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(271, 52);
+            this.btnClick.TabIndex = 6;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // txtUC
+            // 
+            this.txtUC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUC.Location = new System.Drawing.Point(138, 79);
+            this.txtUC.Name = "txtUC";
+            this.txtUC.Size = new System.Drawing.Size(271, 34);
+            this.txtUC.TabIndex = 5;
+            // 
+            // lblUC
+            // 
+            this.lblUC.AutoSize = true;
+            this.lblUC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUC.Location = new System.Drawing.Point(23, 82);
+            this.lblUC.Name = "lblUC";
+            this.lblUC.Size = new System.Drawing.Size(117, 29);
+            this.lblUC.TabIndex = 4;
+            this.lblUC.Text = "Unit Price";
+            // 
+            // txtQ
+            // 
+            this.txtQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQ.Location = new System.Drawing.Point(138, 125);
+            this.txtQ.Name = "txtQ";
+            this.txtQ.Size = new System.Drawing.Size(271, 34);
+            this.txtQ.TabIndex = 3;
+            // 
+            // lblQ
+            // 
+            this.lblQ.AutoSize = true;
+            this.lblQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQ.Location = new System.Drawing.Point(23, 128);
+            this.lblQ.Name = "lblQ";
+            this.lblQ.Size = new System.Drawing.Size(100, 29);
+            this.lblQ.TabIndex = 2;
+            this.lblQ.Text = "Quantity";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(138, 35);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(271, 34);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(23, 38);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(78, 29);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // frmBill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 290);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmBill";
+            this.Text = "Bill";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.TextBox txtUC;
+        private System.Windows.Forms.Label lblUC;
+        private System.Windows.Forms.TextBox txtQ;
+        private System.Windows.Forms.Label lblQ;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ GUI/Lec_6_Q_27/Lec_6_Q_27/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lec_6_Q_27
+{
+    public partial class frmBill : Form
+    {
+        public frmBill()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            int uc = Convert.ToInt32(txtUC.Text);
+            int q = Convert.ToInt32(txtQ.Text);
+
+            int amount = uc * q;
+
+            txtAmount.Text= amount.ToString();
+        }
+    }
+}
