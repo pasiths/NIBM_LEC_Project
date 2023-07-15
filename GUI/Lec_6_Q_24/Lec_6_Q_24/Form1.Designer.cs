b3ae5c605a374410cedc8ec0d90c5226b11ebdc1
Pasith-Home <pasiya20024@gmail.com>
2023-07-15 15:27:53 +0530

    Lec_6_Q_24

---
+++ GUI/Lec_6_Q_24/Lec_6_Q_24/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Lec_6_Q_24
+{
+    partial class frmAddition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnClick);
+            this.panel1.Controls.Add(this.txtNumber2);
+            this.panel1.Controls.Add(this.txtNumber1);
+            this.panel1.Controls.Add(this.lblNumber2);
+            this.panel1.Controls.Add(this.lblNumber1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(409, 204);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber1.Location = new System.Drawing.Point(22, 24);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(119, 29);
+            this.lblNumber1.TabIndex = 0;
+            this.lblNumber1.Text = "Number 1";
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber2.Location = new System.Drawing.Point(22, 72);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(119, 29);
+            this.lblNumber2.TabIndex = 0;
+            this.lblNumber2.Text = "Number 2";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber1.Location = new System.Drawing.Point(147, 24);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(224, 34);
+            this.txtNumber1.TabIndex = 1;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber2.Location = new System.Drawing.Point(147, 72);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(224, 34);
+            this.txtNumber2.TabIndex = 1;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(133, 134);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(126, 46);
+            this.btnClick.TabIndex = 2;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // frmAddition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 204);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmAddition";
+            this.Text = "Addition";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.Label lblNumber1;
+    }
+}
+
---
+++ GUI/Lec_6_Q_24/Lec_6_Q_24/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lec_6_Q_24
+{
+    public partial class frmAddition : Form
+    {
+        public frmAddition()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(txtNumber1.Text);
+            int num2 = Convert.ToInt32(txtNumber2.Text);
+
+            int add=num1 + num2;
+            MessageBox.Show("Addition: " + add);
+        }
+    }
+}
