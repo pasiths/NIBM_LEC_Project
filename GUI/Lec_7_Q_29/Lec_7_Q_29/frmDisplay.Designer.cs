8d7daa3abf12087d1109039764e562e108c7a603
Pasith <pasiths@users.noreply.github.com>
2023-07-27 19:12:17 +0530

    .

--- GUI/Lec_7_Q_29/Lec_7_Q_29/frmDisplay.Designer.cs
+++ GUI/Lec_7_Q_29/Lec_7_Q_29/frmDisplay.Designer.cs
@@ -30,33 +30,48 @@
         {
             this.lblName = new System.Windows.Forms.Label();
             this.lblFS = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(12, 27);
+            this.lblName.Location = new System.Drawing.Point(9, 22);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(0, 29);
+            this.lblName.Size = new System.Drawing.Size(0, 24);
             this.lblName.TabIndex = 0;
             // 
             // lblFS
             // 
             this.lblFS.AutoSize = true;
             this.lblFS.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFS.Location = new System.Drawing.Point(12, 66);
+            this.lblFS.Location = new System.Drawing.Point(9, 54);
+            this.lblFS.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFS.Name = "lblFS";
-            this.lblFS.Size = new System.Drawing.Size(0, 29);
+            this.lblFS.Size = new System.Drawing.Size(0, 24);
             this.lblFS.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmDisplay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 132);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(375, 107);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblFS);
             this.Controls.Add(this.lblName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmDisplay";
             this.Text = "Display";
             this.ResumeLayout(false);
@@ -68,5 +83,6 @@
 
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label lblFS;
+        private System.Windows.Forms.Button button1;
     }
 }
